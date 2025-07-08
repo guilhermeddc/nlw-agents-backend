fcb0390f2813c431a19a37352facdcf4c4b6f471
Guilherme Rodrigues <guilhermeddc@gmail.com>
2025-07-07 21:58:32 -0300

    feat: add rooms listing

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default('postgres://docker:docker@localhost:5432/agents'),
+});
+
+export const env = envSchema.parse(process.env);
